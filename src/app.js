f08f3391348f3429d708731412498e8adc11cf80
LoXur-ux <starkaw@ya.ru>
2023-11-01 20:24:17 +0300

    App add query to DB

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  database: process.env.DB,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
 const logsDir = "logs";
 const logsPath = path.resolve("./logs");
 if (!fs.existsSync(logsPath)) {
@@ -19,8 +28,20 @@ const createdAt = new Date();
 const server = http.createServer((req, res) => {
   fs.appendFileSync(logFilePath, `${new Date().toISOString()}:request\n`);
   res.statusCode = 200;
+
   res.setHeader("Content-Type", "text/plain");
-  res.end(`Hello World, started at ${createdAt.toISOString()}`);
+  res.write(`Hello World, started at ${createdAt.toISOString()}`);
+
+  pool.query("select * from Test", (err, qres) => {
+    if (err) throw err;
+    qres.rows.forEach((row) => {
+      res.write(
+        "id: " + row.id + "| date: " + row.date + "| title: " + row.title
+      );
+    });
+  });
+
+  res.end("А все");
 });
 
 server.listen(port, () => {
